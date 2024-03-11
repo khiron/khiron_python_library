b641e06932324cfcc7a8ed368e86348fe18b1770
richard morris <richard@corkmate.com>
2024-03-12 01:54:07 +1100

    fix function_name

---
+++ {{cookiecutter.library_name}}/tests/test_{{cookiecutter.function_name}}.py
@@ -0,0 +1,5 @@
+import pytest
+from {{cookiecutter.library_name}} import {{cookiecutter.function_name}}
+
+def test_{{cookiecutter.function_name}}():
+    assert {{cookiecutter.function_name}}.{{cookiecutter.function_name}}() == "{{cookiecutter.function_name}}"
--- {{cookiecutter.library_name}}/{{cookiecutter.library_name}}/{{function_name}}.py
+++
@@ -1,2 +0,0 @@
-def {{function_name}}():
-    return "{{function_name}}"
